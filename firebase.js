abb8eb46f40b185c1af0674472e132fd99e4face
Sonika303 <sonikakukreti05@gmail.com>
2026-04-28 18:09:55 +0530

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 if (typeof firebase === 'undefined') throw new Error('[FB] Load Firebase SDK <script> tags BEFORE firebase.js');
 
 if (!firebase.apps.length) firebase.initializeApp({
-  apiKey: 'AIzaSyC_fNfUQUcdhicNNx-e0weEGURbz-mzS8g',
+  apiKey: 'AIzaSyC_fNfUQUcdhicNNx-e0weEGURbz-mZs8g',
   authDomain: 'playconsole4u.firebaseapp.com',
   databaseURL: 'https://playconsole4u-default-rtdb.firebaseio.com',
   projectId: 'playconsole4u',
